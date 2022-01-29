b5765de34b83a5e1f04be5e5cc60581c9ef54d2f
Nicholas Robson <nick@dky.dev>
2022-01-29 18:02:19 +0100

    Add spirit test for PC.

--- goodevilpersongame/Assets/Scripts/SoulController.cs
+++ goodevilpersongame/Assets/Scripts/SoulController.cs
@@ -25,17 +25,9 @@ public class SoulController : MonoBehaviour
 
     void Update()
     {
-        
-        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, layerMask))
-        {
-            transform.position = pc.position + Vector3.ClampMagnitude(hit.point - pc.position, maxDistance);
-        }
-        
-        
-        
-        //transform.position = pc.position + Vector3.ClampMagnitude(Camera.main.ScreenToWorldPoint(Input.mousePosition)  - pc.position, maxDistance);
-        //transform.position = new Vector3(transform.position.x, transform.position.y, pc.position.z-0.02f);
-        
+
+        var point = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+        transform.position = pc.position + Vector3.ClampMagnitude(new Vector3(point.x, point.y, 0) - pc.position, maxDistance);
         
         Debug.DrawLine(transform.position, pc.transform.position, Color.red);
     }
