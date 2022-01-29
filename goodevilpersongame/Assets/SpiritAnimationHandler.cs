409260c0ed4417d7445ab5bec46eff4cdc2debdb
Nicholas Robson <nick@dky.dev>
2022-01-29 16:44:32 +0100

    Add particle effects library.

--- goodevilpersongame/Assets/SpiritAnimationHandler.cs
+++ goodevilpersongame/Assets/SpiritAnimationHandler.cs
@@ -1,14 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
-using Spine.Unity;
 using UnityEngine;
 
-public class SpiritAnimationHandler : MonoBehaviour
-{
+public class SpiritAnimationHandler : MonoBehaviour {
+    public Transform target;
+
     private void Start() {
-        var skeleton = GetComponent<SkeletonAnimation>();
-        skeleton.AnimationState.SetAnimation(0, "idle_body", true);
-        skeleton.AnimationState.SetAnimation(2, "idle_mouth", true);
-        skeleton.AnimationState.SetAnimation(1, "idle_head", true);
+        // var skeleton = GetComponent<SkeletonAnimation>();
+        // skeleton.AnimationState.SetAnimation(0, "idle_body", true);
+        // skeleton.AnimationState.SetAnimation(2, "idle_mouth", true);
+        // skeleton.AnimationState.SetAnimation(1, "idle_head", true);
     }
+
+    private void FixedUpdate() { }
 }
